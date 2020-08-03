1fe5e7c9afe3b6d521ecfed4ddeb1c9f18c48d94
Vagan Mkrtchyan <v.mkrtchyan@i-sys.ru>
2020-08-03 16:09:13 +0400

    Удалил мусор

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
+
+import React, { useEffect, useRef } from 'react';
+import WebViewer from '@pdftron/webviewer'
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+  const viewer = useRef(null);
+  const type = 'docx'
+  const downloadUrl = 'http://z3x.systemz.io/api/fs/file/914d3d01-2953-4003-8952-a9f27bc17ba5'
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        initialDoc: `${downloadUrl}`,
+        fullAPI: true,
+        filename: `print.${type}`,
+      },
+      viewer.current
+    ).then(instance => {
+
+      instance.closeElements(['leftPanel', 'header']);
+    })
+  }, []);
+
+  return (
+    <div className="MyComponent">
+      <div className="header">React sample</div>
+      <div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
+    </div>
+  );
+};
+
+
 
 ReactDOM.render(
   <React.StrictMode>
